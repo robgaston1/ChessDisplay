ba2fd2caad83910e8e8f9887fda101871a04471c
robgaston1 <robcoach0221@gmail.com>
2017-01-24 20:25:19 -0500

    added id letter and number to each grid square

--- chess.js
+++ chess.js
@@ -16,17 +16,55 @@ $(document).ready(function() {
     };
     
     for (var i=1; i<=64; i++) {
+        var numId, letterId, grid;
+        //generate the 0 through 7 label for the grid ID
+        if (i < 8){
+            numId = i;
+        } else if (i % 8 == 0){
+            numId = 8;
+        } else {
+            numId = (i % 8);
+        }
+        //create the letter portion of the grid ID
+        var row = Math.ceil(i/8);
+        switch(row){
+            case 1:
+                letterId = "h";
+                break;
+            case 2:
+                letterId = "g";
+                break;
+            case 3:
+                letterId = "f";
+                break;
+            case 4:
+                letterId= "e";
+                break;
+            case 5:
+                letterId = "d";
+                break;
+            case 6:
+                letterId = "c";
+                break;
+            case 7:
+                letterId = "b";
+                break;
+            case 8:
+                letterId = "a";
+        }
+        grid = letterId + numId;
+        
         if ((Math.ceil(i/8)) % 2 == 1) {
             if (i % 2 == 0) {
-                $('#board').append('<div class="square wht"><div>');
+                $('#board').append('<div id='+grid+' class="square blk"><div>');
             } else {
-                $('#board').append('<div class="square blk"><div>');
+                $('#board').append('<div id='+grid+' class="square wht"><div>');
             }
         } else {
             if (i % 2 == 0) {
-                $('#board').append('<div class="square blk"><div>');
+                $('#board').append('<div id='+grid+' class="square wht"><div>');
             } else {
-                $('#board').append('<div class="square wht"><div>');
+                $('#board').append('<div id='+grid+' class="square blk"><div>');
             }
         }
     }
